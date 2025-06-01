2174690f0e3355138e11f1b28ed5d3a93845c4e3
luizotavioautomacao <luizotavioautomacao@gmail.com>
2025-06-01 20:04:40 -0300

    feat: add repository to index page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import styles from './index.module.scss';
 
+const list = [
+  {
+    title: 'Repositório',
+    label: 'Github',
+    url: 'https://github.com/luizotavioautomacao/vivotv.shop',
+  },
+];
+
+
 const Home: React.FC = () => {
   return (
     <div className={styles.container}>
@@ -8,6 +17,17 @@ const Home: React.FC = () => {
       <p className={styles.subtitle}>
         Sistema de autenticação com JWE | Backend NestJS + Jest | Frontend Next.js + SCSS + Jest
       </p>
+
+      <div className={styles.cardGrid}>
+        {list.map(({ title, label, url }) => (
+          <div key={label} className={styles.card}>
+            <h3>{title}</h3>
+            <a href={url} target="_blank" rel="noopener noreferrer" className={styles.button}>
+              {label}
+            </a>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
