12937c8059fcddcce0e018b9e8ac840193c4d404
luizotavioautomacao <luizotavioautomacao@gmail.com>
2025-06-01 20:05:35 -0300

    update: add swagger on navbar

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -12,19 +12,21 @@ export const Navbar: React.FC = () => {
         <li className={`${styles.navItem} ${styles.desktop}`} onClick={() => router.push('/')}>🏠 Home</li>
         <li className={`${styles.navItem} ${styles.desktop}`} onClick={() => router.push('/login')}>🔐 Login</li>
         <li className={`${styles.navItem} ${styles.desktop}`} onClick={() => router.push('/decrypt')}>🔓 Decrypt</li>
-        <li className={`${styles.navItem} ${styles.desktop}`} onClick={() => router.push('/history')}>📜 History</li>
-        <li className={`${styles.navItem} ${styles.desktop}`}>
+        <li className={`${styles.navItem} ${styles.desktop}`} onClick={() => router.push('/history')}>🔍 History</li>
+        <li className={`${styles.navItem} ${styles.desktop}`} onClick={() => router.push('/api')}>📚 Swagger</li>
+        {/* <li className={`${styles.navItem} ${styles.desktop}`}>
           <a href="https://github.com/luizotavioautomacao/vivotv.shop" target="_blank" rel="noopener noreferrer" className={styles.link}>🧠 Repository</a>
-        </li>
+        </li> */}
 
         {/* Mobile version */}
         <li className={`${styles.navItem} ${styles.mobile}`} onClick={() => router.push('/')}>Home</li>
         <li className={`${styles.navItem} ${styles.mobile}`} onClick={() => router.push('/login')}>Login</li>
         <li className={`${styles.navItem} ${styles.mobile}`} onClick={() => router.push('/decrypt')}>Decrypt</li>
         <li className={`${styles.navItem} ${styles.mobile}`} onClick={() => router.push('/history')}>History</li>
-        <li className={`${styles.navItem} ${styles.mobile}`}>
+        <li className={`${styles.navItem} ${styles.mobile}`} onClick={() => router.push('/api')}>Swagger</li>
+        {/* <li className={`${styles.navItem} ${styles.mobile}`}>
           <a href="https://github.com/luizotavioautomacao/vivotv.shop" target="_blank" rel="noopener noreferrer" className={styles.link}>Repo</a>
-        </li>
+        </li> */}
       </ul>
     </nav>
   );
